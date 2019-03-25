6e0cfb239ef12c72ebfbffdbca9c9825854636ed
chenyusong <chenjw@chinacaring.com>
2019-03-25 20:16:14 +0800

    3/25个人中心按钮

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div id="area"></div>
+    <div id="personal" @click="toPersonal">
+      <i class="icon iconfont icongerenzhongxin1"></i>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+// import func from './vue-temp/vue-editor-bridge';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data: function() {
+    return {};
+  },
+  methods:{
+    toPersonal:function(){
+      // ths.$router.push()
+      console.log("!23");
+    }
+  },
+  mounted:function(){
+    this.book = new Epub("http://140.143.24.96/epubBook/8720238.epub");
+    this.rendition = this.book.renderTo('read', {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      method: 'default'
+    })
+    this.rendition.display()
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
+@import url("//at.alicdn.com/t/font_1101334_eiees6olxs4.css");
+// #app {
+//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   // text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  #personal {
+    position: fixed;
+    bottom: 40px;
+    left: 40px;
+    height:60px;
+    width:60px;
+    border-radius: 50%;
+    border: solid #c8c7cc 2upx;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0 0 10px rgba(102, 102, 102, 0.6);
+    i {
+      padding: 10px;
+      font-size: 40px;
+      color: rgb(22, 194, 194);
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Epub from 'epubjs'
 
 Vue.config.productionTip = false
+global.ePub = Epub
+Vue.use(Epub)
 
 new Vue({
   render: h => h(App),
